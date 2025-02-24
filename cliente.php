b11752836bd4e306656a5e82ec01a172ce87d3d8
Maicol Loaiza Rodriguez <nicolmaicol9@cotecnova.edu.co>
2025-02-24 00:08:05 -0500

    Se-crea-comunicación-cliente servidor

--- cliente.php
+++ cliente.php
@@ -1,3 +1,20 @@
 <?php
-	
+$options = array( 
+    'location' => 'http://localhost/webservices/appwebservices/server.php',
+    'uri' => 'http://localhost/webservices/appwebservices/'
+); 
+
+$client = new SoapClient(NULL, $options);
+
+$nombre = "Usuario"; 
+
+echo $client->saludar($nombre . '!!') . "</br>"; 
+
+$num1 = 10;
+$num2 = 5;
+
+echo "El resultado de la suma es: " . $client->suma($num1, $num2) . "</br>"; 
+echo "El resultado de la resta es: " . $client->resta($num1, $num2) . "</br>"; 
+echo "El resultado de la multiplicación es: " . $client->multiplicacion($num1, $num2) . "</br>"; 
+echo "El resultado de la división es: " . $client->division($num1, $num2) . "</br>"; 
 ?>
--- server.php
+++ server.php
@@ -1,3 +1,46 @@
-<?php
-	
+<?php 
+require('conexion.php'); 
+
+class serverSoap extends Conexion 
+{
+    public function saludar($name)
+    {
+        return 'Hola, '.$name.'!'; 
+    }  
+
+    public function suma($num1, $num2)
+    {
+        return $num1 + $num2;  
+    } 
+
+    public function resta($num1, $num2)
+    {
+        return $num1 - $num2;  
+    } 
+
+    public function multiplicacion($num1, $num2)
+    {
+        return $num1 * $num2;  
+    } 
+
+    public function division($num1, $num2)
+    {
+        if ($num2 == 0) {
+            return "Error: División por cero.";
+        }
+        return $num1 / $num2;  
+    } 
+}
+
+// Crear un nuevo servidor SOAP 
+$options = array('uri' => 'http://localhost/webservices/appwebservices/'); 
+
+// Instanciar el servidor SOAP 
+$server = new SoapServer(NULL, $options);
+
+// Configurar qué clase manejará las solicitudes SOAP 
+$server->setClass('serverSoap'); 
+
+// Procesar las solicitudes SOAP 
+$server->handle();
 ?>
